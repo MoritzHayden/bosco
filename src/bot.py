cad7a197d64ae62d8206100372d543e9715cc620
Hayden Moritz <Moritz.Hayden@gmail.com>
2023-04-18 07:36:26 -0500

    Add command stubs

--- src/bot.py
+++ src/bot.py
@@ -13,22 +13,38 @@ intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 tree = app_commands.CommandTree(client)
 
+# Ready event
 @client.event
 async def on_ready():
     print("Bot Ready!")
 
 # Sync command
-@tree.command(name = "sync", description = "Syncs the command tree (admin only)")
+@tree.command(name = "sync",
+              description = "Admin: Syncs the command tree")
 async def first_command(ctx):
     if ctx.user.id in utils.get_admins():
         await tree.sync()
-        await ctx.response.send_message("Synced command tree!")
+        await ctx.response.send_message("Successfully synced command tree.")
     else:
-        await ctx.response.send_message("Error: Only admins can run this command!")
+        await ctx.response.send_message("Sorry, only admins can run this command.")
 
 # Ping command
-@tree.command(name = "ping", description = "Pings the bot and returns latency")
+@tree.command(name = "ping",
+              description = "Pings the bot and returns latency")
 async def first_command(ctx):
     await ctx.response.send_message(f'Pong! Latency: {round(client.latency*1000)}ms')
 
+# Deep Dives command
+@tree.command(name = "deep-dives",
+              description = "Returns details about the weekly deep dives")
+async def first_command(ctx):
+    await ctx.response.send_message(f'Coming soon!')
+
+# Fun Fact command
+@tree.command(name = "fun-fact",
+              description = "Returns one or more fun facts")
+async def first_command(ctx, count=1):
+    await ctx.response.send_message(f'Coming soon!')
+
+# Run client
 client.run(TOKEN)
