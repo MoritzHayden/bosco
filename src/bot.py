f8adfb215a63f8ad6435e91f17004d0f5a11a27e
Hayden Moritz <Moritz.Hayden@gmail.com>
2023-04-19 10:17:20 -0500

    Update presence

--- src/bot.py
+++ src/bot.py
@@ -21,7 +21,7 @@ tree = app_commands.CommandTree(client)
 # Ready event
 @client.event
 async def on_ready():
-    await client.change_presence(status=discord.Status.online, activity=discord.game('Deep Rock Galactic'))
+    await client.change_presence(status=discord.Status.online, activity=discord.Game(name='Deep Rock Galactic'))
     print("Bot Ready!")
 
 
