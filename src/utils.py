4e6159de668f4622094c08a2d8d8fda1a785df73
Hayden Moritz <Moritz.Hayden@gmail.com>
2023-04-19 11:41:04 -0500

    Add is_admin command

--- src/utils.py
+++ src/utils.py
@@ -2,10 +2,16 @@ import json
 
 
 # Get a list of authorized users (admins)
-def get_admins():
+def get_admin_ids():
     admins = []
     with open("admins.json") as f:
         data = json.load(f)
         for admin in data['admins']:
             admins.append(str(admin["id"]))
     return admins
+
+def is_admin(id: str):
+    for admin_id in get_admin_ids():
+        if id == admin_id:
+            return True
+    return False
