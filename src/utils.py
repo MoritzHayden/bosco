27c8688b22456e334fe542e5f6210ee06b0bb496
Hayden Moritz <Moritz.Hayden@gmail.com>
2023-04-19 18:50:14 -0500

    Make file path relative

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,12 @@
+import os
 import json
 import random
 
 
 # Return a random salute string
 def get_random_salute():
-    with open("json/salutes.json") as f:
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, 'json/salutes.json')
+    with open(filename) as f:
         data = json.load(f)
         return random.choice(data['salutes'])
