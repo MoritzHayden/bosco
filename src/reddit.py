320833e603a8a2b1667ccb3cb2e08fb3345bdc80
Hayden Moritz <Moritz.Hayden@gmail.com>
2023-04-19 21:31:44 -0500

    Fix indexing

--- src/reddit.py
+++ src/reddit.py
@@ -37,16 +37,16 @@ def parse_details(text: str, date: str, url: str):
     dd_about = lines[4].replace("*", "").split(" | ")
     dd = DeepDive(DeepDiveType.DEEP_DIVE, dd_about[1], dd_about[2], date, url)
     dd_stages = [lines[8].split("|"), lines[9].split("|"), lines[10].split("|")]
-    for i, dd_stage in enumerate(dd_stages):
-        dd.add_stage(i, dd_stage[1], dd_stage[2], dd_stage[3], dd_stage[4])
+    for dd_stage in dd_stages:
+        dd.add_stage(dd_stage[1], dd_stage[2], dd_stage[3], dd_stage[4], dd_stage[5])
     deep_dives.append(dd)
 
     # Elite Deep Dive
     edd_about = lines[12].replace("*", "").split(" | ")
     edd = DeepDive(DeepDiveType.ELITE_DEEP_DIVE, edd_about[1], edd_about[2], date, url)
     edd_stages = [lines[16].split("|"), lines[17].split("|"), lines[18].split("|")]
-    for i, edd_stage in enumerate(edd_stages):
-        edd.add_stage(i, edd_stage[1], edd_stage[2], edd_stage[3], edd_stage[4])
+    for edd_stage in edd_stages:
+        edd.add_stage(edd_stage[1], edd_stage[2], edd_stage[3], edd_stage[4], edd_stage[5])
     deep_dives.append(edd)
 
     return deep_dives
