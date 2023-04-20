be679396cfa92f581bd0907b9b7dfae9ea9fbcf7
Hayden Moritz <Moritz.Hayden@gmail.com>
2023-04-20 06:25:03 -0500

    Update praw query

--- src/reddit.py
+++ src/reddit.py
@@ -12,7 +12,10 @@ def get_deep_dive_details(reddit_client_id: str, reddit_client_secret: str, deep
             user_agent="discord:dev.boscobot",
             check_for_async=False
         )
-        submission = reddit.subreddit("DeepRockGalactic").search(query="Weekly Deep Dives Thread", sort="hot")[0]
+        submissions = reddit.subreddit("DeepRockGalactic").search(query="Weekly Deep Dives Thread", sort="hot", time_filter="week")
+        submission = None
+        for s in submissions:
+            submission = s
         date = parse_date(str(submission.title))
         url = str(submission.url)
         text = str(submission.selftext)
