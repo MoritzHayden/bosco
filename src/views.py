7fa0e2161e5505ce45a77e033463a7a7d7db9763
Hayden Moritz <Moritz.Hayden@gmail.com>
2023-04-20 12:24:20 -0500

    Simplify buttons

--- src/views.py
+++ src/views.py
@@ -4,9 +4,5 @@ import discord
 class ButtonView(discord.ui.View):
     def __init__(self):
         super().__init__(timeout=None)
-        website_button = discord.ui.Button(label="Website", url="https://boscobot.dev/")
-        invite_button = discord.ui.Button(label="Invite to Server", url="https://discord.com/api/oauth2/authorize?client_id=1097476432579539026&permissions=2147568704&scope=bot")
-        website_button.style = discord.ButtonStyle.success
-        invite_button.style = discord.ButtonStyle.primary
-        self.add_item(website_button)
-        self.add_item(invite_button)
+        self.add_item(discord.ui.Button(label="Website", url="https://boscobot.dev/"))
+        self.add_item(discord.ui.Button(label="Invite to Server", url="https://discord.com/api/oauth2/authorize?client_id=1097476432579539026&permissions=2147568704&scope=bot"))
