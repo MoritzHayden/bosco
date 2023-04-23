81d768fe05c315d1f5a05c8823cebb5a5d48eb8d
Hayden Moritz <Moritz.Hayden@gmail.com>
2023-04-22 23:43:27 -0500

    Fix formatting

--- src/config.py
+++ src/config.py
@@ -2,5 +2,6 @@ BOSCO_USER_AGENT: str = 'discord:dev.boscobot'
 BOSCO_WEBSITE_TEXT: str = 'Website'
 BOSCO_WEBSITE_URL: str = 'https://boscobot.dev/'
 BOSCO_INVITE_TEXT: str = 'Invite to Server'
-BOSCO_INVITE_URL: str = 'https://discord.com/api/oauth2/authorize?client_id=1097476432579539026&permissions=2147568704&scope=bot'
+BOSCO_INVITE_URL: str = 'https://discord.com/api/oauth2/authorize' \
+                        '?client_id=1097476432579539026&permissions=2147568704&scope=bot'
 ERROR_RESPONSE_TEXT: str = 'Oops, something went wrong! Please try again later.'
