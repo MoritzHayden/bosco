dad3a16a28f6f6ee823d8939af4d6329656b3bd4
hynrey <hynrey@gmail.com>
2023-04-29 17:24:25 +0500

    Pylint fix "Trailing whitespace" #10 ref

--- src/tests/test_trivia.py
+++ src/tests/test_trivia.py
@@ -14,8 +14,8 @@ def file_trivia():
     filename = os.path.join(os.path.dirname(__file__),'../json/trivia.json')
     with open(file=filename, encoding="utf-8") as f:
         data = f.read()
-        yield json.loads(data)['trivia']  
- 
+        yield json.loads(data)['trivia']
+
 class TestTriviaService():
     # Test file exist
     def test_trivia_file_exists(self):
@@ -27,7 +27,6 @@ class TestTriviaService():
     # Test get one trivia by index
     def test_get_trivia(self, trivia, file_trivia):
         assert trivia.get_trivia(1) == file_trivia[1]
-    # Test get random trivia 
+    # Test get random trivia
     def test_get_random_trivia(self, trivia, file_trivia):
         assert trivia.get_random_trivia() in file_trivia
-    
