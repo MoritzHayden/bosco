a9fd8958aa772e04042a1561bd846511539be8f9
hynrey <hynrey@gmail.com>
2023-04-29 17:17:08 +0500

    Cover TriviaService with tests #10 ref
    Cover all methods of class TriviaService with tests
    Test get_random_trivia func
    Test get_trivia by index func
    Test init of Trivia class
    Test trivia.json file exists

--- src/tests/test_trivia.py
+++ src/tests/test_trivia.py
@@ -1,14 +1,33 @@
 import os
 import json
+import pytest
 from ..service.trivia import TriviaService
-    
-def test_trivia_service_init():
-    # Get trivia from class
-    trivia = TriviaService()
-    # Load trivia from file
+
+@pytest.fixture(autouse=True)
+def trivia():
+    # setUp TriviaService
+    yield TriviaService()
+
+@pytest.fixture(autouse=True)
+def file_trivia():
+    # SetUp trivias from file
     filename = os.path.join(os.path.dirname(__file__),'../json/trivia.json')
     with open(file=filename, encoding="utf-8") as f:
         data = f.read()
-        loaded_trivia = json.loads(data)['trivia']
-
-    assert trivia.trivia == loaded_trivia
+        yield json.loads(data)['trivia']  
+ 
+class TestTriviaService():
+    # Test file exist
+    def test_trivia_file_exists(self):
+        path = "../json/trivia.json"
+        assert os.path.isfile(path)
+    # Test for TriviaService init
+    def test_trivia_service_init(self, trivia, file_trivia):
+        assert trivia.trivia == file_trivia
+    # Test get one trivia by index
+    def test_get_trivia(self, trivia, file_trivia):
+        assert trivia.get_trivia(1) == file_trivia[1]
+    # Test get random trivia 
+    def test_get_random_trivia(self, trivia, file_trivia):
+        assert trivia.get_random_trivia() in file_trivia
+    
