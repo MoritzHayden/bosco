5018735b7cf4bd53970982f728b4cafc5319299c
Hayden Moritz <Moritz.Hayden@gmail.com>
2023-04-30 12:08:31 -0500

    Add test script

---
+++ src/scripts/test.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Set install flag to false
+install_flag=false
+
+# Print help message
+print_help() {
+    echo "Run Pytest on all test files."
+    echo " "
+    echo "Usage: ./test.sh [options]"
+    echo " "
+    echo "Options:"
+    echo "-h        show brief help"
+    echo "-i        intall dependencies"
+    exit 0
+}
+
+# Parse command line arguments
+while getopts 'ih' flag; do
+    case "${flag}" in
+    i) install_flag=true ;;
+    h) print_help ;;
+    *) print_help ;;
+    esac
+done
+
+# Move to src directory
+cd "$(dirname "$(readlink -f "$0")")/.."
+
+# Run the install script with dev dependencies if the flag is set
+if [ "$install_flag" = true ]; then
+    echo "Running install script..."
+    source ./scripts/install.sh -d
+fi
+
+# Run Pytest
+echo "Running Pytest..."
+pipenv run pytest tests/
