dbe0ddefb423def0444cd05c37aa817d1a62bbd7
hynrey <hynrey@gmail.com>
2023-05-08 12:22:44 +0500

    Review fixes
    remove __init__ files
    move test files to folder
    change relative import of modules

---
+++ src/tests/service/test_trivia.py
@@ -0,0 +1,35 @@
+import os
+import json
+import pytest
+from service.trivia import TriviaService
+
+@pytest.fixture(name="trivia",autouse=True)
+def fixture_trivia():
+    # setUp TriviaService
+    return TriviaService()
+
+@pytest.fixture(name="file_trivia",autouse=True)
+def fixture_file_trivia():
+    # SetUp trivias from file
+    filename = os.path.join(os.path.dirname(__file__),'../../json/trivia.json')
+    with open(file=filename, encoding="utf-8") as f:
+        return json.load(f)['trivia']
+
+class TestTriviaService:
+    # Test for TriviaService init
+    def test_trivia_service_init(self, trivia, file_trivia):
+        assert isinstance(trivia, TriviaService)
+        assert isinstance(trivia.trivia, list)
+        assert trivia.trivia == file_trivia
+
+    # Test get one trivia by index
+    def test_get_trivia(self, trivia, file_trivia):
+        assert isinstance(trivia.trivia, list)
+        assert isinstance(trivia.get_trivia(1), str)
+        assert trivia.get_trivia(1) == file_trivia[1]
+
+    # Test get random trivia
+    def test_get_random_trivia(self, trivia, file_trivia):
+        assert isinstance(trivia.trivia, list)
+        assert isinstance(trivia.get_random_trivia(), str)
+        assert trivia.get_random_trivia() in file_trivia
