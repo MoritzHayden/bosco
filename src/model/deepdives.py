8d707ca0f471c148ac238cf4e8aded897cfe6cf6
Bartosz Dokurno <bartusekcraft@gmail.com>
2023-09-15 14:43:16 +0200

    rename get_emoji module to emoji

--- src/model/deepdives.py
+++ src/model/deepdives.py
@@ -1,7 +1,7 @@
 from enum import Enum
 from typing import Optional
 from pydantic import BaseModel, ConfigDict
-from util.get_emoji import get_emoji
+from util.emoji import get_emoji
 
 
 class DiveType(str, Enum):
